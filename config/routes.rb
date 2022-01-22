263c4978fa02aa457a174cd02976a33d597c6dbd
Mike <mike.alibo@bitcontroltech.com>
2022-01-22 12:13:53 +0800

    added the custom registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations',
+    unlocks: 'users/unlocks'
+  }
+
   root 'home#index'
 end
